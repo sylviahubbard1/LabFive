e1cd90d8e52f3b153848bece1e028e3c39a1fd84
sylviahubbard1 <sylviahubbard@gmail.com>
2016-10-19 10:47:45 -0400

    Add project files.

---
+++ LabThree/LabThreeClassLibrary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ClassLibrary1
+{
+    public class CalculateSquareAndCube
+    {
+        public int SquareNumber(int number)
+        {
+            return number * number;
+        }
+        public int CubeNumber(int num)
+        {
+            return num * num * num;
+        }
+    }
+}
---
+++ LabThree/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ans;
+            do
+            {
+                Console.WriteLine("Learn your Square and Cubes!");
+                Console.Write("Enter an integer: ");
+                int numRepeat = int.Parse(Console.ReadLine());
+                CalculateSquareAndCube cal = new CalculateSquareAndCube();
+                Console.WriteLine("Number\t\tSquared\t\tCubed");
+                Console.WriteLine("======\t\t=====\t\t======");
+                for (int i = 1; i <= numRepeat; i++)
+                {
+                    Console.WriteLine($"{i} \t\t {cal.SquareNumber(i)} \t\t{cal.CubeNumber(i)}");
+                }
+                Console.WriteLine("Continue?(y/n)");
+                ans = Console.ReadLine();
+            } while (ans == "y");
+
+
+            //Console.WriteLine(cal.SquareNumber(numRepeat));   
+        }
+    }
+}
